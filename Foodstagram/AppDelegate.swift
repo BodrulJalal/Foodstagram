5f54c75d7c32f3ebde7b7bcfe525b02e54551aa7
BodrulJalal <bodrul.jalal31@myhunter.cuny.edu>
2022-04-27 11:23:52 -0400

    Parse server details added
    Using the back4app parse server, added the client ID and the app key to sync with the accounts

--- Foodstagram/AppDelegate.swift
+++ Foodstagram/AppDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Parse
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -14,6 +15,15 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        let parseConfig = ParseClientConfiguration {
+                    $0.applicationId = "FovurOgUVJKJdAibdpJCYSsvPPh87Dp6UltTP07g" // <- UPDATE
+                    $0.clientKey = "eh6OfMH3QVkyCahAUTDJd5UPfzSlu0ORgrwKfPoW" // <- UPDATE
+                    $0.server = "https://parseapi.back4app.com"
+            }
+            Parse.initialize(with: parseConfig)
+        
+        
         return true
     }
 

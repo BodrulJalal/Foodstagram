7c31d2353a55e578f06cc53933cfc44ebc8f1e46
BodrulJalal <bodrul.jalal31@myhunter.cuny.edu>
2022-04-28 17:39:17 -0400

    Feed is complete and you can view posts

--- Foodstagram/FeedViewController.swift
+++ Foodstagram/FeedViewController.swift
@@ -6,14 +6,67 @@
 //
 
 import UIKit
+import Parse
+import AlamofireImage
 
-class FeedViewController: UIViewController {
+class FeedViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    @IBOutlet weak var tableView: UITableView!
+    
+    var posts = [PFObject]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        
 
         // Do any additional setup after loading the view.
     }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        let query = PFQuery(className:"Posts")
+        query.includeKey("author")
+        query.limit = 20
+        
+        query.findObjectsInBackground { (posts, error) in
+            if posts != nil {
+                self.posts = posts!
+                self.tableView.reloadData()
+            }
+        }
+    }
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return posts.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "PostCell") as! PostCell
+        
+        let post = posts[indexPath.row]
+        
+        //let abc = post["User"] as! PFUser
+        //cell.usernameLabel.text = abc.username
+    
+        
+        cell.captionLabel.text = post["Recipe_and_Comments"] as? String
+        cell.dishLabel.text = post["Dish_Name"] as? String
+        
+        let imageFile = post["Photo"] as! PFFileObject
+        let urlString = imageFile.url!
+        let url = URL(string: urlString)!
+        
+        cell.photoView.af.setImage(withURL: url)
+        
+        
+        return cell
+    }
+    
     
 
     /*
---
+++ Foodstagram/PostCell.swift
@@ -0,0 +1,29 @@
+//
+//  PostCell.swift
+//  Foodstagram
+//
+//  Created by Bodrul on 4/28/22.
+//
+
+import UIKit
+
+class PostCell: UITableViewCell {
+
+    @IBOutlet weak var photoView: UIImageView!
+    @IBOutlet weak var captionLabel: UILabel!
+    @IBOutlet weak var dishLabel: UILabel!
+    
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
